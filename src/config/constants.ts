6c37b7cb5926ef9c81d51fbf8d5d67e0207f486c
Alex Lin <allienx@hashmail.cc>
2020-11-08 16:54:09 -0800

    Better code organization

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,9 @@
 export const FontFamilies = {
   MONOSPACE: "'JetBrains Mono', 'Menlo', 'Consolas', monospace",
 }
+
+export const COPY_CLIPBOARD_SUCCESS_FLASH_MS = 1250
+export const EMAIL_DOMAIN = 'hashmail.cc'
+export const NUM_RANDOM_EMAILS = 1000
+export const TEXT_LOOP_INTERVAL_MS = 50
+export const TEXT_SPIN_DURATION_MS = 1000
--- src/util/generateRandomHashes.ts
+++ src/util/generateRandomHashes.ts
@@ -1,27 +1,36 @@
-import { DigestAlgorithm } from './digestMessage'
-import { generateRandomHash } from './generateRandomHash'
+import { nanoid } from 'nanoid'
+import { DigestAlgorithm, digestMessage } from './digestMessage'
+
+type HashId = {
+  id: string
+  hash: string
+}
 
 type Options = {
-  count?: number
+  count: number
   algorithm?: DigestAlgorithm
 }
 
-export async function generateRandomHashes(
-  options: Options = {},
-): Promise<string[]> {
-  const { count, algorithm } = {
-    count: 1000,
-    algorithm: DigestAlgorithm.sha256,
-    ...options,
-  }
-
-  const hashes: string[] = []
+export async function generateRandomHashes({
+  count,
+  algorithm = DigestAlgorithm.sha256,
+}: Options): Promise<HashId[]> {
+  const hashIds: HashId[] = []
 
   for (let i = 0; i < count; i++) {
-    const hash = await generateRandomHash(algorithm)
+    const hashId = await generateRandomHash(algorithm)
 
-    hashes.push(hash)
+    hashIds.push(hashId)
   }
 
-  return hashes
+  return hashIds
+}
+
+async function generateRandomHash(
+  algorithm: DigestAlgorithm = DigestAlgorithm.sha256,
+): Promise<HashId> {
+  const id = nanoid()
+  const hash = await digestMessage(id, algorithm)
+
+  return { id, hash }
 }
